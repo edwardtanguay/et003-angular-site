6ed4cde63d31c9aaf7f231f3637bf44301123bed
Edward Tanguay (windows) <edward@tanguay.info>
2022-11-24 11:28:09 +0100

    first box works

--- src/app/info-box/info-box.component.ts
+++ src/app/info-box/info-box.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,Input } from '@angular/core';
 
 @Component({
   selector: 'app-info-box',
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./info-box.component.scss']
 })
 export class InfoBoxComponent {
-
+  @Input() title = 'Info Box';
+  @Input() body = '(content coming soon...)';
 }
